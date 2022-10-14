2d911160c8661e600ee921fb8361529b798f1871
Roman Lysenko <romashkaxxxl@gmail.com>
2022-10-14 17:45:20 +0300

    add task

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import { Component } from "react";
+import { Searchbar } from "./Searchbar/Searchbar";
+import { Gallery } from './ImageGallery/ImageGallery'
+
+export class App extends Component {
+  state = {
+    searchQuery: '',
+  };
+  
+  handleFormSubmit = searchQuery => {
+    this.setState({ searchQuery });
+  };
+render(){
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      <Searchbar onSubmit={this.handleFormSubmit} />
+      <Gallery searchQuery={ this.state.searchQuery} />
+    </div>
+  );
+}};
